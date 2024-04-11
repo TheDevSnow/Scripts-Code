db2d687435f71551c867e3c6138e34c5cde23506
TheDevSnow <160942726+TheDevSnow@users.noreply.github.com>
2024-04-11 15:59:08 -0400

    Add files via upload

--- cookie clicker.py
+++ cookie clicker.py
@@ -1,16 +1,34 @@
 import turtle
 
+print("Fullscreen is required.")
+
 wn = turtle.Screen()
 wn.title("Cookie Clicker mini-game by @TheDevSnow")
 wn.bgcolor("black")
 
 wn.register_shape(r"C:\Users\iCan Student\Desktop\Python\cookie.gif")
+wn.register_shape(r"C:\Users\iCan Student\Desktop\Python\cursor.gif")
+wn.register_shape(r"C:\Users\iCan Student\Desktop\Python\grandma.gif")
 
 cookie = turtle.Turtle()
 cookie.shape(r"C:\Users\iCan Student\Desktop\Python\cookie.gif")
 cookie.speed(0)
 
+cursor = turtle.Turtle()
+cursor.shape(r"C:\Users\iCan Student\Desktop\Python\cursor.gif")
+cursor.speed(0)
+cursor.penup()
+cursor.goto(300, 150)
+
+grandma = turtle.Turtle()
+grandma.shape(r"C:\Users\iCan Student\Desktop\Python\grandma.gif")
+grandma.speed(0)
+grandma.penup()
+grandma.goto(500, -200)
+
 clicks = 0
+increment = 1
+grandmawork = 0
 
 pen = turtle.Turtle()
 pen.hideturtle()
@@ -18,22 +36,45 @@ pen.color("white")
 pen.penup()
 pen.goto(0, 200)
 pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
+pen.goto(-250, 50)
+pen.write(f"Cursors: {increment}", align="center", font=("Courier New", 20, "normal"))
+pen.goto(-250, 0)
+pen.write(f"Grandmas: {clicks}", align="center", font=("Courier New", 20, "normal"))
 
 def clicked(x, y):
-    global clicks
-    clicks += 1
-    if clicks >= 100:
-        clicks += 2-1
-    elif clicks >= 500: 
-        clicks += 5-1
-    elif clicks >= 10000:
-        clicks >= 10-1
-    elif clicks >= 100000:
-        clicks >= 1000-1
+    global clicks, increment, grandmawork
+    clicks += increment
     pen.clear()
+    pen.penup()
+    pen.goto(0, 200)
     pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
 
 
+def upgrade(x, y):
+    global clicks, increment
+    if clicks >= 50:
+        clicks -= 50
+        increment += 1
+        pen.clear()
+        pen.penup()
+        pen.goto(-250, 50)
+        pen.write(f"Cursors: {increment}", align="center", font=("Courier New", 20, "normal"))
+        
+    elif clicks < 50:
+        pen.penup()
+        pen.goto(0,0)
+        pen.pendown()
+        pen.write(f"You do not have enough cookies to buy this upgrade", align="center", font=("Courier New", 20, "normal"))
+        pen.penup()
+
+
+def grandmaupgrade():
+
+
+
 cookie.onclick(clicked)
+cursor.onclick(upgrade)
 
 wn.mainloop()
+
+
