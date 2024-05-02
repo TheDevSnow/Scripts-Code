269bca1081e2977294358c0a5d3de984afd17baa
TheDevSnow <6204572@portal.rsb.qc.ca>
2024-05-02 14:34:42 -0400

    Cleared Lua branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# Scripts-Code
-I will put some scripts to help with coding or just to do stuff easier.
--- week 4.py
+++
@@ -1,9 +0,0 @@
-def invword():
-    Userlist = input("Enter what you want to reverse: ")
-    reverse = ""
-    for i in range(len(Userlist)):
-        reverse += Userlist [len(Userlist) -1 -i]
-    print (reverse)
-
-
-invword()
